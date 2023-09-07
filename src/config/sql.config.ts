c6d9f4d3570a31277679c726e186fd56fdc6a8f3
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-07 23:59:04 +0300

    подключить часть конфигураций

--- src/config/sql.config.ts
+++ src/config/sql.config.ts
@@ -1,3 +1,9 @@
 import { registerAs } from '@nestjs/config';
+import { Dialect } from 'sequelize';
 
-export const sqlConfig = registerAs('database', () => ({}));
+export const sqlConfig = registerAs('database', () => ({
+    dialect: <Dialect>process.env.SQL_DIALECT || 'mysql',
+    logging: process.env.SQL_LOGGING === 'true' ? true : false,
+    
+
+}));
