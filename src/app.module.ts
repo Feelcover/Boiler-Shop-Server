db56101305e13259eb4d137af2f434b4f57030d4
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-08 00:17:11 +0300

    подключить все конфигурации в app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { databaseConfig } from './config/configuration';
+import { SequelizeConfigService } from './config/sequelizeConfig.seevice';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports:[ConfigModule],
+      useClass: SequelizeConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    UsersModule
+  ],
 })
 export class AppModule {}
