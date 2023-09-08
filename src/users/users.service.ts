5646753fabf7406eef08e3dab91c6de5d3ce6c45
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-08 16:22:26 +0300

    заинжектить модель User и создать метод findOne

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './model/users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel(User)
+        private userModel: typeof User,
+    ) {}
+
+    findOne(filter: {
+        where: { id?: string, email?: string, username?: string }
+    }):Promise<User> {
+        return this.userModel.findOne({...filter});
+    }
+}
