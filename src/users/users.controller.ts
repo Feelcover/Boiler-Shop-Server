0c65f167b4334b61bf2d33e7132cfa00ce72f212
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-09 13:42:10 +0300

    создать метод контроллера для создания пользователя

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private UsersService: UsersService) {}
+
+  @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-Type', 'application/json')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.UsersService.create(createUserDto);
+  }
+}
