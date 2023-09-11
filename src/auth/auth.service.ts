ddba0df393a89ce55016e7697a92145b2f227915
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-11 16:19:35 +0300

    реализовать проверку авторизации пользователя validateUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersService.findOne({ where: { username } });
+
+    if (!user) {
+      throw new UnauthorizedException('Неверные учетные данные');
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordValid) {
+      throw new UnauthorizedException('Неверные учетные данные');
+    }
+    if (user && passwordValid) {
+      return {
+        userId: user.id,
+        username: user.username,
+        email: user.email,
+      };
+    }
+    return null;
+  }
+}
