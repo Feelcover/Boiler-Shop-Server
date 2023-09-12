8b04cba42842a0e6142df9fe07de1fd00079150b
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-12 22:20:20 +0300

    реализовать ведение документации через Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,16 @@ async function bootstrap() {
   );
   app.use(passport.initialize());
   app.use(passport.session());
+  const config = new DocumentBuilder()
+    .setTitle('Boiler')
+    .setDescription('api documentation')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
