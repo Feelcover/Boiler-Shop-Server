cc081f79c148450814a01eb6daed078a6c8a6ec1
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-12 22:43:43 +0300

    дополнить документацию swagger для эндпоинтов

--- src/users/types/index.ts
+++ src/users/types/index.ts
@@ -14,7 +14,7 @@ export class SigninUserResponse {
       user: {
         userId: 1,
         username: 'Andrey',
-        email: 'qwe123',
+        email: 'example@example.com',
       },
     },
   })
@@ -26,4 +26,42 @@ export class SigninUserResponse {
 
   @ApiProperty({ example: 'Успешная авторизация' })
   message: string;
+}
+
+export class LogoutUserResponse {
+  @ApiProperty({ example: 'Вы вышли из аккаунта' })
+  message: string;
+}
+
+export class SigninCheckResponse {
+  @ApiProperty({ example: '1' })
+  userId: string;
+
+  @ApiProperty({ example: 'Andrey' })
+  username: string;
+
+  @ApiProperty({ example: 'example@example.com' })
+  email: string;
+}
+
+export class SignupResponse {
+  @ApiProperty({ example: '1' })
+  id: string;
+
+  @ApiProperty({ example: 'Andrey' })
+  username: string;
+
+  @ApiProperty({ example: 'example@example.com' })
+  email: string;
+
+  @ApiProperty({
+    example: '$2b$10$rCuoJcDkg.4..JlelLPwF.T0pHX9HVGI.pJDrW9GA5olNrNNdQqGe',
+  })
+  password: string;
+
+  @ApiProperty({ example: '2023-09-10 21:26:54' })
+  updatedAt: string;
+
+  @ApiProperty({ example: '2023-09-10 21:26:53' })
+  createdAt: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,13 +13,20 @@ import { ApiBody, ApiResponse } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
-import { SigninUserRequest, SigninUserResponse } from './types';
+import {
+  LogoutUserResponse,
+  SigninCheckResponse,
+  SigninUserRequest,
+  SigninUserResponse,
+  SignupResponse,
+} from './types';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private UsersService: UsersService) {}
 
+  @ApiResponse({ type: SignupResponse })
   @Post('/signup')
   @HttpCode(HttpStatus.CREATED)
   @Header('Content-Type', 'application/json')
@@ -27,8 +34,8 @@ export class UsersController {
     return this.UsersService.create(createUserDto);
   }
 
-  @ApiBody({type: SigninUserRequest})
-  @ApiResponse({type: SigninUserResponse})
+  @ApiBody({ type: SigninUserRequest })
+  @ApiResponse({ type: SigninUserResponse })
   @Post('/signin')
   @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
@@ -36,15 +43,17 @@ export class UsersController {
     return { user: req.user, message: 'Успешная авторизация' };
   }
 
+  @ApiResponse({ type: SigninCheckResponse })
   @Get('/signin-check')
   @UseGuards(AuthGuard)
   signinCheck(@Request() req) {
     return req.user;
   }
 
+  @ApiResponse({ type: LogoutUserResponse })
   @Get('/logout')
   logout(@Request() req) {
     req.session.destroy();
-    return {message: "Вы вышли из аккаунта"};
+    return { message: 'Вы вышли из аккаунта' };
   }
 }
