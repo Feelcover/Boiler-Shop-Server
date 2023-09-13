8548b63ca9be3038dec8ff7c13c8afaee2b09ac0
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-13 16:34:11 +0300

    создать модель BoilerPartsModel и подключить в модуле

--- src/boiler-parts/boiler-parts.module.ts
+++ src/boiler-parts/boiler-parts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { BoilerPartsController } from './boiler-parts.controller';
 import { BoilerPartsService } from './boiler-parts.service';
 
 @Module({
+  imports: [SequelizeModule.forFeature([BoilerPartsModule])],
   controllers: [BoilerPartsController],
-  providers: [BoilerPartsService]
+  providers: [BoilerPartsService],
+  exports: [BoilerPartsService],
 })
 export class BoilerPartsModule {}
