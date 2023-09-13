d5c579e9b551d1e489453c96701a12e3d7d36a44
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-13 23:25:11 +0300

    реализовать методы new, findOne, findOneByName, searchByString

--- src/boiler-parts/boiler-parts.service.ts
+++ src/boiler-parts/boiler-parts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { BoilerPartsModel } from './model/boiler-parts.model';
 import { IBoilerPartsQuery } from './types';
+import { Op } from 'sequelize';
 
 @Injectable()
 export class BoilerPartsService {
@@ -24,6 +25,33 @@ export class BoilerPartsService {
   async bestsellers(): Promise<{ count: number; rows: BoilerPartsModel[] }> {
     return this.boilerPartsModel.findAndCountAll({
       where: { bestsellers: true },
+    });
+  }
+
+  async new(): Promise<{ count: number; rows: BoilerPartsModel[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      where: { new: true },
+    });
+  }
+
+  async findOne(id: number | string): Promise<BoilerPartsModel> {
+    return this.boilerPartsModel.findOne({
+      where: { id },
+    });
+  }
+
+  async findOneByName(name: string): Promise<BoilerPartsModel> {
+    return this.boilerPartsModel.findOne({
+      where: { name },
+    });
+  }
+
+  async searchByString(
+    str: string,
+  ): Promise<{ count: number; rows: BoilerPartsModel[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      limit: 20,
+      where: { name: { [Op.like]: `%${str}%` } },
     });
   }
 }
