66dde9f59d0c67a45cb212e7f0a0dec33726511c
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-13 16:36:55 +0300

    пофиксить подключение модели BoilerPartsModel

--- src/boiler-parts/boiler-parts.module.ts
+++ src/boiler-parts/boiler-parts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { BoilerPartsController } from './boiler-parts.controller';
 import { BoilerPartsService } from './boiler-parts.service';
+import { BoilerPartsModel } from './model/boiler-parts.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([BoilerPartsModule])],
+  imports: [SequelizeModule.forFeature([BoilerPartsModel])],
   controllers: [BoilerPartsController],
   providers: [BoilerPartsService],
   exports: [BoilerPartsService],
