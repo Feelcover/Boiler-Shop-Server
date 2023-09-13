0a5c5e1fe64c126173812013afffb3119ca0e40b
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-13 23:21:06 +0300

    реализовать paginateAndFilter и bestsellers

--- src/boiler-parts/boiler-parts.service.ts
+++ src/boiler-parts/boiler-parts.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { BoilerPartsModel } from './model/boiler-parts.model';
+import { IBoilerPartsQuery } from './types';
 
 @Injectable()
-export class BoilerPartsService {}
+export class BoilerPartsService {
+  constructor(
+    @InjectModel(BoilerPartsModel)
+    private boilerPartsModel: typeof BoilerPartsModel,
+  ) {}
+
+  async paginateAndFilter(
+    query: IBoilerPartsQuery,
+  ): Promise<{ count: number; rows: BoilerPartsModel[] }> {
+    const limit = +query.limit;
+    const offset = +query.offset * 20;
+    return this.boilerPartsModel.findAndCountAll({
+      limit,
+      offset,
+    });
+  }
+
+  async bestsellers(): Promise<{ count: number; rows: BoilerPartsModel[] }> {
+    return this.boilerPartsModel.findAndCountAll({
+      where: { bestsellers: true },
+    });
+  }
+}
